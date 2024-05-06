8bf24ed6b29b3c0ad9782fcadfd32f030a708b54
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-05-05 23:30:11 -0300

    Sistema de rotas (teste)

--- index.php
+++ index.php
@@ -5,10 +5,25 @@ require_once ("./repository/ProductRepository.php");
 require_once ("./NovaEstante.php");
 
 
-$pdo = new PDO("sqlite:" . __DIR__ . "/db.sqlite");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$produtoRepository = new ProductRepository($pdo);
+//$pdo = new PDO("sqlite:" . __DIR__ . "/db.sqlite");
+//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//$produtoRepository = new ProductRepository($pdo);
+//$estante = new NovaEstante($pdo);
 
-$estante = new NovaEstante($pdo);
-$estante->gerarEstante();
 
+$rota = $_SERVER['REQUEST_URI'];
+
+// Verifica a rota e inclui a view correspondente
+switch ($rota) {
+    case '/mostrarPessoa':
+        $produto_nome = "Teste";
+        require_once("./views/mostrarPessoa.php");
+        break;
+    case '/mostrarOla':
+        require_once("./views/mostrarOla.php");
+        break;
+    // Adicione mais rotas conforme necessário
+    default:
+        echo "Rota não encontrada";
+        break;
+}
---
+++ views/mostrarPessoa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Olá pessoa!</p>
+
+    <p>O nome do produto é: <?php echo $produto_nome ?></p>
+</body>
+
+</html>
